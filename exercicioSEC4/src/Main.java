8a1a19d0d142690bc35fd0a031f687f5bbe370ae
victorchavesm <victorhcmnz@gmail.com>
2023-08-16 17:37:50 -0300

    exercicio

---
+++ exercicioSEC4/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//pedir comprimento e largura pra calcular a área:
+		System.out.print("Digite a largura do terreno: ");
+		double largura = sc.nextDouble();
+		System.out.print("Digite o comprimento do terreno: ");
+		double comprimento = sc.nextDouble();
+		double area = largura * comprimento;
+		System.out.printf("A área do terreno é %.1f m2. %n", area);
+		
+		//pedir o preço do metro2 pra calcular o valor do terreno:
+		System.out.print("Digite o preço do metro quadrado: ");
+		double metroQuadrado = sc.nextDouble();
+		double preco = area * metroQuadrado;
+		System.out.printf("O preço do terreno é R$ %.2f", preco);
+		
+		sc.close();
+	}
+
+}
