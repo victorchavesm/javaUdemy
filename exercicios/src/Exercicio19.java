2d2d51986169a58e6ed412441e17d6592d609526
victorchavesm <victorhcmnz@gmail.com>
2023-08-23 20:06:44 -0300

    exercicios

--- exercicios/src/Exercicio19.java
+++ exercicios/src/Exercicio19.java
@@ -1,9 +1,22 @@
+//Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+//essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
+
+import java.util.Scanner;
 
 public class Exercicio19 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
