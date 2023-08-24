ebcd739b2476b48d5794e0a71d77a6d4578b3ea3
victorchavesm <victorhcmnz@gmail.com>
2023-08-24 18:12:41 -0300

    Exercicios

--- exercicios/src/Exercicio24.java
+++ exercicios/src/Exercicio24.java
@@ -1,9 +1,26 @@
+//Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+//começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+//exemplo.
+
+import java.util.Scanner;
 
 public class Exercicio24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro positivo: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+		}
+		
+		sc.close();
 	}
-
 }
